84c6f8c1fffb1034ed0ad38f70a34749e4611c57
TinchoGithub <perezjmartin12@gmail.com>
2024-03-14 16:40:06 -0300

    Agrego funcionalidad para editar productos (UPDATE)

--- src/main/java/com/proyectoecommerce/controller/ErrorController.java
+++ src/main/java/com/proyectoecommerce/controller/ErrorController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-
+/*
     @RequestMapping("/error")
     public String manejarError(){
         return "error/error";
     }
+
+ */
 }
