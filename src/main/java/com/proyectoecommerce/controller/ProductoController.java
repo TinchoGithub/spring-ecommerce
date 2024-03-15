e7ec956eb8c694653dfcc860fea5a291c2948e4d
TinchoGithub <perezjmartin12@gmail.com>
2024-03-15 01:43:53 -0300

    Metodo para guardar la imágen del producto

--- src/main/java/com/proyectoecommerce/controller/ProductoController.java
+++ src/main/java/com/proyectoecommerce/controller/ProductoController.java
@@ -3,11 +3,13 @@ package com.proyectoecommerce.controller;
 import com.proyectoecommerce.model.ProductoModel;
 import com.proyectoecommerce.model.UsuarioModel;
 import com.proyectoecommerce.service.ProductoService;
+import com.proyectoecommerce.service.UploadImagenService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
@@ -16,6 +18,7 @@ import java.util.Optional;
 public class ProductoController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductoController.class);
+    private UploadImagenService uploadImagenService;
 
     @Autowired
     private ProductoService productoService;
@@ -31,10 +34,23 @@ public class ProductoController {
     }
 
     @PostMapping("/save")
-    public String save(ProductoModel producto){
+    public String save(ProductoModel producto,@RequestParam("img") MultipartFile file){
         logger.info("Este es el objeto producto {}",producto);
         UsuarioModel usuario = new UsuarioModel(1L,"","","","","","","");
         producto.setUsuario(usuario);
+
+        if (producto.getId() == null){
+            String nombreImagen = uploadImagenService.saveImage(file);
+            producto.setImagen(nombreImagen);
+        } else if (file.isEmpty()) {
+            ProductoModel p = new ProductoModel();
+            p = productoService.get(producto.getId()).get();
+            producto.setImagen(p.getImagen());
+
+        }else {
+            String nombreImagen = uploadImagenService.saveImage(file);
+            producto.setImagen(nombreImagen);
+        }
 
         productoService.save(producto);
         return "redirect:/productos";
