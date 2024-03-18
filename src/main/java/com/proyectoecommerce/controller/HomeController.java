0ab722a04fdaa5afe15550945ecc8812139b7a1a
TinchoGithub <perezjmartin12@gmail.com>
2024-03-17 23:42:59 -0300

    Agrego vista de carrito d ecompras

--- src/main/java/com/proyectoecommerce/controller/HomeController.java
+++ src/main/java/com/proyectoecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -36,6 +37,12 @@ public class HomeController {
         producto = productoModelOptional.get();
         model.addAttribute("producto", producto);
         return "usuario/producto_home";
+    }
+
+    @PostMapping("/cart")
+    public String addcart(){
+
+        return "usuario/carrito";
     }
 
 }
