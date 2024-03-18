4ea5d2d01a1370098c669b081543a356df4cd932
TinchoGithub <perezjmartin12@gmail.com>
2024-03-17 23:02:04 -0300

    Agregando vista del producto al usuario

--- src/main/java/com/proyectoecommerce/controller/HomeController.java
+++ src/main/java/com/proyectoecommerce/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.proyectoecommerce.controller;
 
 import com.proyectoecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoService productoService;
@@ -19,6 +24,11 @@ public class HomeController {
 
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Long id){
+        log.info("id producto enviado como parametro {}", id);
+        return "usuario/producto_home";
     }
 
 }
