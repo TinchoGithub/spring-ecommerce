080ac6306db6dab66e68433784cfbe3bbbc5485f
TinchoGithub <perezjmartin12@gmail.com>
2024-03-20 23:25:53 -0300

    Agrego vista para visualizar resumen de la orden de compra

--- src/main/java/com/proyectoecommerce/controller/HomeController.java
+++ src/main/java/com/proyectoecommerce/controller/HomeController.java
@@ -105,4 +105,10 @@ public class HomeController {
         return "/usuario/carrito";
     }
 
+    @GetMapping("/orden")
+    public String verOrden(){
+
+        return "usuario/resumenOrden";
+    }
+
 }
